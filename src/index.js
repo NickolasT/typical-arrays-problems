cec4ff97b737f4d2a8b0ce0fce342d6ed079f3f3
NikolasT <nick33tr>
2020-03-05 17:34:46 +0200

    feat: add solution to start typical arrays problem task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,31 @@
 
 exports.min = function min (array) {
-  return 0;
+  if ( array === undefined || array.length < 1 ) return 0;
+  let min = array [ 0 ]
+  for ( let i = 1; i < array.length; i++ )
+    if ( min > array [ i ] ) {
+      min = array [ i ]
+    }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if ( array === undefined || array.length < 1 ) return 0;
+  let max = array [ 0 ]
+  for ( let i = 1; i < array.length; i++ )
+    if ( max < array [ i ] ) {
+      max = array [ i ]
+    }
+  return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if ( array === undefined || array.length < 1 ) return 0;
+  let avg;
+  let sum = array [ 0 ];
+  for ( let i = 1; i < array.length; i++ ) {
+    sum = sum + array [ i ];
+    avg = sum / array.length
+  }
+  return avg;
 }
